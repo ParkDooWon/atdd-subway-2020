f71a774c96717e0b6cacb1bf433bc8fc814ee0b0
ParkDooWon <pass345@naver.com>
2020-08-06 16:14:42 +0900

    feat: 거리별 fare에 대한 인수테스트 작성

--- src/main/java/wooteco/subway/maps/map/dto/PathResponseAssembler.java
+++ src/main/java/wooteco/subway/maps/map/dto/PathResponseAssembler.java
@@ -1,12 +1,12 @@
 package wooteco.subway.maps.map.dto;
 
-import wooteco.subway.maps.map.domain.SubwayPath;
-import wooteco.subway.maps.station.domain.Station;
-import wooteco.subway.maps.station.dto.StationResponse;
-
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import wooteco.subway.maps.map.domain.SubwayPath;
+import wooteco.subway.maps.station.domain.Station;
+import wooteco.subway.maps.station.dto.StationResponse;
 
 public class PathResponseAssembler {
     public static PathResponse assemble(SubwayPath subwayPath, Map<Long, Station> stations) {
@@ -16,6 +16,6 @@ public class PathResponseAssembler {
 
         int distance = subwayPath.calculateDistance();
 
-        return new PathResponse(stationResponses, subwayPath.calculateDuration(), distance);
+        return new PathResponse(stationResponses, subwayPath.calculateDuration(), distance, 0);
     }
 }
--- src/test/java/wooteco/subway/maps/map/acceptance/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/maps/map/acceptance/PathAcceptanceTest.java
@@ -1,19 +1,20 @@
 package wooteco.subway.maps.map.acceptance;
 
-import com.google.common.collect.Lists;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static wooteco.subway.maps.line.acceptance.step.LineStationAcceptanceStep.*;
+import static wooteco.subway.maps.map.acceptance.step.PathAcceptanceStep.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.Lists;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import wooteco.subway.common.acceptance.AcceptanceTest;
 import wooteco.subway.maps.line.acceptance.step.LineAcceptanceStep;
 import wooteco.subway.maps.line.dto.LineResponse;
 import wooteco.subway.maps.station.acceptance.step.StationAcceptanceStep;
 import wooteco.subway.maps.station.dto.StationResponse;
-
-import static wooteco.subway.maps.line.acceptance.step.LineStationAcceptanceStep.지하철_노선에_지하철역_등록되어_있음;
-import static wooteco.subway.maps.map.acceptance.step.PathAcceptanceStep.*;
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
@@ -21,6 +22,10 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private Long 강남역;
     private Long 양재역;
     private Long 남부터미널역;
+    private Long 목동역;
+    private Long 오목교역;
+    private Long 신정역;
+
     private Long 이호선;
     private Long 신분당선;
     private Long 삼호선;
@@ -41,6 +46,9 @@ public class PathAcceptanceTest extends AcceptanceTest {
         강남역 = 지하철역_등록되어_있음("강남역");
         양재역 = 지하철역_등록되어_있음("양재역");
         남부터미널역 = 지하철역_등록되어_있음("남부터미널역");
+        목동역 = 지하철역_등록되어_있음("목동역");
+        오목교역 = 지하철역_등록되어_있음("오목교역");
+        신정역 = 지하철역_등록되어_있음("신정역");
 
         이호선 = 지하철_노선_등록되어_있음("2호선", "GREEN");
         신분당선 = 지하철_노선_등록되어_있음("신분당선", "RED");
@@ -48,13 +56,48 @@ public class PathAcceptanceTest extends AcceptanceTest {
 
         지하철_노선에_지하철역_등록되어_있음(이호선, null, 교대역, 0, 0);
         지하철_노선에_지하철역_등록되어_있음(이호선, 교대역, 강남역, 2, 2);
+        지하철_노선에_지하철역_등록되어_있음(이호선, 강남역, 오목교역, 10, 5);
 
         지하철_노선에_지하철역_등록되어_있음(신분당선, null, 강남역, 0, 0);
         지하철_노선에_지하철역_등록되어_있음(신분당선, 강남역, 양재역, 2, 1);
+        지하철_노선에_지하철역_등록되어_있음(신분당선, 양재역, 목동역, 50, 10);
+        지하철_노선에_지하철역_등록되어_있음(신분당선, 목동역, 신정역, 10, 7);
 
         지하철_노선에_지하철역_등록되어_있음(삼호선, null, 교대역, 0, 0);
         지하철_노선에_지하철역_등록되어_있음(삼호선, 교대역, 남부터미널역, 1, 2);
         지하철_노선에_지하철역_등록되어_있음(삼호선, 남부터미널역, 양재역, 2, 2);
+    }
+
+    @DisplayName("거리별 요금을 계산한다.")
+    @Test
+    void distanceFareTest() {
+        //when
+        ExtractableResponse<Response> response1 = 거리_경로_조회_요청("DURATION", 2L, 6L);
+        ExtractableResponse<Response> response2 = 거리_경로_조회_요청("DURATION", 1L, 6L);
+        ExtractableResponse<Response> response3 = 거리_경로_조회_요청("DURATION", 3L, 5L);
+        ExtractableResponse<Response> response4 = 거리_경로_조회_요청("DURATION", 2L, 5L);
+        ExtractableResponse<Response> response5 = 거리_경로_조회_요청("DURATION", 2L, 7L);
+
+        //then
+        적절한_경로를_응답(response1, Lists.newArrayList(강남역, 오목교역));
+        총_거리와_소요_시간을_함께_응답함(response1, 10, 5);
+        validateFare(response1, 1250);
+
+        적절한_경로를_응답(response2, Lists.newArrayList(교대역, 강남역, 오목교역));
+        총_거리와_소요_시간을_함께_응답함(response2, 12, 7);
+        validateFare(response2, 1350);
+
+        적절한_경로를_응답(response3, Lists.newArrayList(양재역, 목동역));
+        총_거리와_소요_시간을_함께_응답함(response3, 50, 10);
+        validateFare(response3, 2050);
+
+        적절한_경로를_응답(response4, Lists.newArrayList(강남역, 양재역, 목동역));
+        총_거리와_소요_시간을_함께_응답함(response4, 52, 11);
+        validateFare(response4, 2150);
+
+        적절한_경로를_응답(response5, Lists.newArrayList(강남역, 양재역, 목동역, 신정역));
+        총_거리와_소요_시간을_함께_응답함(response5, 62, 18);
+        validateFare(response5, 2250);
     }
 
     @DisplayName("두 역의 최단 거리 경로를 조회한다.")
